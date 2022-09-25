39e5957ff5334340e51e0139cc83b96ab33ae3e7
Amber Hogarth <amberhogarth@hotmail.com>
2022-09-25 19:57:07 +1000

    Update score status program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,14 +3,27 @@ CP1404 Practical 1 - Debugging
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
+import random
 
-if score < 0 or score > 100:
-    print("Invalid score.")
-else:
-    if 90 <= score <= 100:
-        print("Excellent!")
-    elif 50 <= score < 90:
-        print("Pass.")
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_score_status(score))
+    random_score = random.randint(0, 101)
+    print("Random score: {}".format(random_score))
+    print(determine_score_status(random_score))
+
+
+def determine_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score."
     else:
-        print("Bad.")
+        if 90 <= score <= 100:
+            return "Excellent!"
+        elif 50 <= score < 90:
+            return "Pass."
+        else:
+            return "Bad."
+
+
+main()
