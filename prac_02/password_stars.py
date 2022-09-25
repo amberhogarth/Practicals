da73378aeca6317cbc435604f047d085271724ad
Amber Hogarth <amberhogarth@hotmail.com>
2022-09-25 19:21:40 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,25 @@
 """
 Program to check password length print asterisks
 """
-password = input("Password: ")
-min_length = 4
 
-while len(password) < min_length:
-    print("Invalid length!")
+
+def main():
+    password = get_password()
+
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Password: ")
+    min_length = 4
+    while len(password) < min_length:
+        print("Invalid length!")
+        password = input("Password: ")
+    return password
 
-print("*" * len(password))
+
+main()
