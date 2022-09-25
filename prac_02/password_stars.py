c5f99cd32e4c157e8f7b1494747531ea41cf5d1e
Amber Hogarth <amberhogarth@hotmail.com>
2022-09-25 20:14:08 +1000

    Update function docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -10,10 +10,12 @@ def main():
 
 
 def print_asterisks(password):
+    """Prints a line of asterisks based on the length of the password"""
     print("*" * len(password))
 
 
 def get_password():
+    """Gets password input and verifies it meets length requirements"""
     password = input("Password: ")
     min_length = 4
     while len(password) < min_length:
