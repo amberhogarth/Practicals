bde4f1f5cefb5d0a7585b44fdf5871c56409c8c0
Amber Hogarth <amberhogarth@hotmail.com>
2022-10-19 16:04:17 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,18 +1,20 @@
 """
-CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
+CP1404 Prac 5 - Amber Hogarth - State Names
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+        state_code = input("Enter short state: ").upper()
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+        state_code = input("Enter short state: ").upper()
+
+# 4.
+for i in CODE_TO_NAME:
+    print(f"{i:3} is {CODE_TO_NAME[i]}")
