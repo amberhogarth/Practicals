b1adfa524698122b50d67118a0df7165fc763911
Amber Hogarth <amberhogarth@hotmail.com>
2022-11-10 07:01:18 +1000

    Remove commented-out code

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,15 +1,11 @@
-"""
 
-"""
-
-from datetime import date, datetime
+from datetime import datetime
 
 
 class Project:
 
     def __init__(self, name, start_date, priority=0, cost_estimate=0.0, completion=0):
         self.name = name
-        # self.start_date = start_date
         self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
