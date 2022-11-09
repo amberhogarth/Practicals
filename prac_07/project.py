2afa9c455ec2c52214817ae13c74f49aa3796488
Amber Hogarth <amberhogarth@hotmail.com>
2022-11-10 07:00:01 +1000

    Complete Project Management activity

---
+++ prac_07/project.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+from datetime import date, datetime
+
+
+class Project:
+
+    def __init__(self, name, start_date, priority=0, cost_estimate=0.0, completion=0):
+        self.name = name
+        # self.start_date = start_date
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion = completion
+
+    def __repr__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, " \
+               f"completion: {self.completion}%"
+
+    def is_complete(self):
+        return self.completion == 100
